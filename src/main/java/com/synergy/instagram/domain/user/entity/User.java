95e28ce4496d3dd1992cdde26380e45e94076923
taemin <jungtaemin1214@naver.com>
2025-05-07 19:19:07 +0900

    User 엔티티 생성, InstagramApplication에 @EnableJpaAuditing 추가

---
+++ src/main/java/com/synergy/instagram/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.synergy.instagram.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(String username, String password, String email, LocalDateTime createdAt) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
